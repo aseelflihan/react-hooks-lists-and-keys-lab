f78d019b1500afedbd75300f87fddda26f2e025c
aseelflihan <aseelflihan@gmail.com>
2023-03-19 23:40:39 +0300

    complate assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navLinks = links.map(link => (
+    <a href={`#${link}`} key={link}>{link}</a>
+  ))
+  return <nav>{/* display an <a> tag for each link here */}
+    {navLinks}
+  </nav>;
 }
 
 export default NavBar;
